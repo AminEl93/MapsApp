341d9342f6615628f6e79261508681d9c63393e9
AminEl93 <amin_11_iverson@hotmail.com>
2023-11-29 13:17:38 +0100

    Añadir marcadores de forma dinámica en el mapa

---
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,53 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
+
+@Component({
+    templateUrl: './markers-page.component.html',
+    styleUrls: ['./markers-page.component.css']
+})
+
+export class MarkersPageComponent {
+    
+    @ViewChild('map') divMap?: ElementRef;
+
+    public map?: Map;
+    public currentLngLat: LngLat = new LngLat(2.159941673535286, 41.38693544873411);
+
+    ngAfterViewInit(): void {  
+        if (!this.divMap) throw 'El elemento HTML no fue encontrado';    
+        this.map = new Map({
+            container: this.divMap.nativeElement,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: this.currentLngLat,
+            zoom: 13
+        });
+
+        // Crear marcadores personalizados
+        /* 
+        const markerHtml = document.createElement('div');
+        markerHtml.innerHTML = 'Amin El Meziani'
+        const marker = new Marker({
+            color: 'red',
+            element: markerHtml
+        }).setLngLat(this.currentLngLat).addTo(this.map);
+        */
+    }
+
+    // Crear un marcador en el mapa
+    createMarker() {
+        if (!this.map) return;    
+        // Generar un color hexadecimal de manera aleatoria
+        const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+        const lngLat = this.map.getCenter();    
+        this.addMarker(lngLat, color);
+    }
+
+    // Añadir un marcador en el mapa
+    addMarker(lngLat: LngLat, color: string) {
+        if (!this.map) return;    
+        const marker = new Marker({
+            color: color,
+            draggable: true
+        }).setLngLat(lngLat).addTo(this.map);    
+    }
+}
