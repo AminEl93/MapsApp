622818113354f815088a80e86152f8bf12e80b19
AminEl93 <amin_11_iverson@hotmail.com>
2023-11-30 12:35:43 +0100

    Standalone Component: Componentes reutilizables

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
     standalone: true,
+    imports: [CounterAloneComponent],
     templateUrl: './alone-page.component.html'
 })
 
 export class AlonePageComponent {
-
+    constructor() { }
 }
